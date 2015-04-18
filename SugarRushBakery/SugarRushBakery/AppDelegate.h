6eafc93fcf84aeeec72a0970595833ef6c89555e
sergizzle <sgfelix11@hotmail.com>
2015-04-17 18:17:20 -0700

    Initialized application

---
+++ SugarRushBakery/SugarRushBakery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SugarRushBakery
+//
+//  Created by Sergio Felix on 4/17/15.
+//  Copyright (c) 2015 Super Nova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SugarRushBakery/SugarRushBakery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SugarRushBakery
+//
+//  Created by Sergio Felix on 4/17/15.
+//  Copyright (c) 2015 Super Nova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
